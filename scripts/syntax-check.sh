b5fc9e6488675f60b0a38a38f4550b0f028dd92c
tommyxie2026-tech <tommyxie2026@gmail.com>
2026-05-26 00:54:01 +0900

    test: include encryption and cert maintenance playbooks

--- scripts/syntax-check.sh
+++ scripts/syntax-check.sh
@@ -9,12 +9,15 @@ PLAYBOOKS=(
   0001-download-binaries.yml
   0000-common-service.yml
   0002-common-kubeconfig.yml
+  0003-encryption-config.yml
   0005-install-lb.yml
   0010-create-manager-set.yml
   0012-manager-set-kube.yml
   0020-create-compute-set.yml
   0030-install-cni.yml
   0040-etcd-snapshot.yml
+  0400-cert-expiry-check.yml
+  0401-renew-component-certs.yml
   0090-stop-services.yml
   0091-reset-node-runtime.yml
   0099-container-cleanup.yml
